beb655170dcda67f332c6f498b35cebdb991d4d8
jeje2208 <baziinje@gmail.com>
2015-03-28 14:41:56 +0100

    non fini mais en ligne pour secu systeme de verif de connexion etablie non terminé

--- CommServerApi/src/apiCommLobby/Connexion.java
+++ CommServerApi/src/apiCommLobby/Connexion.java
@@ -9,12 +9,18 @@ import org.bukkit.scheduler.BukkitRunnable;
 public class Connexion extends BukkitRunnable{
 	public static Socket s;
 	public static boolean co = false;
+	public static boolean connect = false;
 	
 	@Override
 	public void run() {
 		try {
 			co = true;
 			s = new Socket(InetAddress.getLocalHost(), main.Config.getServerInfo().getInt("lobbyPort"), InetAddress.getLocalHost(), main.Config.getServerInfo().getInt("port"));
+			if(!(s == null)){
+				connect = true;
+			}
+			System.out.println(connect+"");
+			s.toString();
 			new ReceiveMessage(s).runTaskAsynchronously(main.Main.getPlugin());
 			Bukkit.getLogger().info("Connexion avec le lobby effectue sur le port  " + s.getPort());
 		} catch (Exception e) {
