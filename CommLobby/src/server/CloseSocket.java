d7509264a8ff2197aebfbb3603892e3b525b39cd
jeje2208 <baziinje@gmail.com>
2015-03-30 15:01:27 +0200

    Modification du message de deconnexion d'un serveur

--- CommLobby/src/server/CloseSocket.java
+++ CommLobby/src/server/CloseSocket.java
@@ -7,7 +7,7 @@ public class CloseSocket {
 	public static void close(String server){
 		try {
 			Socket s = main.Main.listServer.get(server);
-			System.out.println("deconnexion d un serveur");
+			System.out.println(server + " vient de se deconnecter, il etait sur le port  " + s.getPort());
 			s.close();
 			main.Main.listServer.remove(server);
 		} catch (Exception e) {
