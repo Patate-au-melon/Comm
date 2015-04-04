1813e91c017bff05769301c0162bae4541251527
jeje2208 <baziinje@gmail.com>
2015-04-04 12:13:14 +0200

    amelioration mineur

--- CommLobby/src/signControl/Update.java
+++ CommLobby/src/signControl/Update.java
@@ -1,8 +1,18 @@
 package signControl;
 
+import java.net.Socket;
+import java.util.Map.Entry;
+
+import org.bukkit.Bukkit;
+
+
 public class Update {
 	
 	public Update(){
+		for(Entry<String, Socket> entry : main.Main.listServer.entrySet()) {
+			String server = entry.getKey();
+			new server.SendMessage(server, "sign", "update").send();
+		}
 		
 	}
 	
@@ -10,6 +20,18 @@ public class Update {
 		if(main.Main.listServer.containsKey(server)){
 			new server.SendMessage(server, "sign", "update").send();
 		}
+	}
+	
+	public static void boucle(){
+		Bukkit.getScheduler().scheduleSyncDelayedTask(main.Main.getPlugin(), new Runnable(){
+
+			@Override
+			public void run() {
+				new Update();
+				
+			}
+			
+		}, 2400);
 	}
 
 }
