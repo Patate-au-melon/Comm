2bc302df184a40f52259ecb04a41636632db831a
jeje2208 <baziinje@gmail.com>
2015-04-04 12:04:30 +0200

    Amélioration de systeme de gestion des panneaux cote lobby

--- CommLobby/src/demarageSetup/Main.java
+++ CommLobby/src/demarageSetup/Main.java
@@ -4,11 +4,13 @@ import java.net.Socket;
 import java.util.HashMap;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 
 public class Main {
 	
 	public static void setup(){
 		main.Main.listServer = new HashMap<String, Socket>();
+		signControl.Receive.listSign = new HashMap<Location, String[]>();
 		new server.Start().runTaskAsynchronously(main.Main.getPlugin());
 		baseDeDonnee.Connexion.connect();
 		RecupConfigListServerName.recupListServerName();
--- CommLobby/src/signControl/Receive.java
+++ CommLobby/src/signControl/Receive.java
@@ -1,5 +1,8 @@
 package signControl;
 
+import java.util.HashMap;
+import java.util.Map.Entry;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -11,6 +14,7 @@ public class Receive {
 
 	private Location loc;
 	private String[] line;
+	public static HashMap<Location, String[]> listSign;
 	
 	public Receive(String server, String label, String message){
 		if(label.equalsIgnoreCase("sign")){
@@ -23,7 +27,16 @@ public class Receive {
 				this.loc = new Location(w, x, y, z);
 				for(int i = 0; i<4;i++)
 					line[i] = msg[i+4];
-				updateSign(this.loc, this.line);
+				if(loc.getChunk().isLoaded()){
+					updateSign(loc, line);
+				}else{
+					if(listSign.containsKey(loc)){
+						listSign.replace(loc, line);
+					}else{
+						listSign.put(loc, line);
+					}
+					boucle();
+				}
 			}
 		}
 	}
@@ -37,6 +50,26 @@ public class Receive {
 			sign.update();
 			System.out.println("mise a jour du panneau en pos x:"+ loc.getBlockX() + " pos y "+ loc.getBlockY()+ " pos z" +loc.getBlockZ());
 		}
+	}
+	
+	private void boucle(){
+		Bukkit.getScheduler().scheduleSyncDelayedTask(main.Main.getPlugin(), new Runnable(){
+
+			@Override
+			public void run() {
+				for(Entry<Location, String[]> entry : listSign.entrySet()) {
+				    Location loc = entry.getKey();
+				    String[] line = entry.getValue();
+				    if(loc.getChunk().isLoaded()){
+				    	updateSign(loc, line);
+				    }
+				}
+				if(listSign.isEmpty() == false){
+					boucle();
+				}
+			}
+			
+		}, 40);
 	}
 
 }
