d1debb9bc9cdce3ef7ebff23180efaddc496528f
jeje2208 <baziinje@gmail.com>
2015-04-14 21:25:10 +0200

    d

--- CommServerApi/src/signControl/Send.java
+++ CommServerApi/src/signControl/Send.java
@@ -3,11 +3,7 @@ package signControl;
 public class Send {
 	
 	public Send(String line){
-		String world = main.Config.getSignControl().getString("world");
-		String x = main.Config.getSignControl().getString("x");
-		String y = main.Config.getSignControl().getString("y");
-		String z = main.Config.getSignControl().getString("z");
-		String msg = world +" "+ x +" "+ y +" "+ z+" "+ line;
+		String msg = line;
 		apiCommLobby.SendMessage.sendToLobby("sign", msg);
 	}
 
