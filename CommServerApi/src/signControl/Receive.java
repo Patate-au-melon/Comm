ff0c2d6f042bbbbd77ceee18125d3afba4b67e51
jeje2208 <baziinje@gmail.com>
2015-04-27 18:47:39 +0200

    suppr sytsem.out.println

--- CommServerApi/src/signControl/Receive.java
+++ CommServerApi/src/signControl/Receive.java
@@ -3,15 +3,12 @@ package signControl;
 public class Receive {
 	
 	public Receive(String server, String label, String message){
-		System.out.println(label);
-		System.out.println(message);
 		if(label.equalsIgnoreCase("sign")){
 			if(message.equalsIgnoreCase("update")){
 				new Send("aze:aze a z e");
 			}
 		}
 		if(label.equalsIgnoreCase("sendJoueur")){
-			System.out.println("yolo");
 			apiCommLobby.SendMessage.sendToLobby("sendJoueur", message);
 		}
 	}
