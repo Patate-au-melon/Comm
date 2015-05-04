96b7e549ce23d31da89eabc195d560505eddce50
jeje2208 <baziinje@gmail.com>
2015-05-04 20:39:08 +0200

    Changement de base de donnee

--- CommServerApi/src/demarageSetup/Main.java
+++ CommServerApi/src/demarageSetup/Main.java
@@ -3,9 +3,9 @@ package demarageSetup;
 public class Main {
 	
 	public static void setup(){
-		String url = "jdbc:mysql://mysql-hbct.alwaysdata.net/hbct_plugin";
-		String user = "hbct_plugin";
-		String password = "jeje";
+		String url = "jdbc:mysql://publicsql-1.pulseheberg.net/service_35502";
+		String user = "service_35502";
+		String password = "V7f9ebUn31";
 		main.Api.BdDconnect(url, user, password);
 		CreateConfig.createServerInfo();
 		new apiCommLobby.Connexion().runTaskAsynchronously(main.Main.getPlugin());
