4bed1279f16d5449f707999f82f080bf61b3f998
jeje2208 <baziinje@gmail.com>
2015-05-26 20:12:40 +0200

    modif pour aller sur le pvp box

--- CommServerApi/src/joueur/Message.java
+++ CommServerApi/src/joueur/Message.java
@@ -2,15 +2,26 @@ package joueur;
 
 import java.util.ArrayList;
 
+import org.bukkit.Bukkit;
+
 public class Message {
 	
 	public static void sendJoueur(String server, ArrayList<String> message){
 		if(message.get(0).equalsIgnoreCase("sendJoueur")){
-			ArrayList<String> msg = new ArrayList<>();
-			msg.add("transfertJoueur");
-			msg.add(message.get(1));
-			msg.add(message.get(2));
-			commServer.SendMessage.sendToLobby(msg);
+			if(Bukkit.getOnlinePlayers().size() <= Bukkit.getMaxPlayers()){
+				ArrayList<String> msg = new ArrayList<>();
+				msg.add("transfertJoueur");
+				msg.add(message.get(1));
+				msg.add(message.get(2));
+				commServer.SendMessage.sendToLobby(msg);
+			}else{
+				ArrayList<String> msg = new ArrayList<>();
+				msg.add("sendMessageJoueur");
+				msg.add(message.get(1));
+				msg.add(message.get(2));
+				msg.add("§4Le serveur est complet, merci d'attendre qu'une place ce libère");
+				commServer.SendMessage.sendToLobby(msg);
+			}
 		}
 	}
 
--- CommServerApi/src/signControl/UpdateSign.java
+++ CommServerApi/src/signControl/UpdateSign.java
@@ -2,6 +2,7 @@ package signControl;
 
 import java.util.ArrayList;
 
+import org.bukkit.Bukkit;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
@@ -18,10 +19,10 @@ public class UpdateSign {
 	public static void updateSign(){
 		ArrayList<String> msg = new ArrayList<>();
 		msg.add("signControl Update"); //On dit que l'on veut update un sign
-		msg.add("aze"); //Line 1
-		msg.add("aze"); //line 2
-		msg.add("aze"); //Line 3
-		msg.add("aze"); //Line 4
+		msg.add("§1§lPvP - Box"); //Line 1
+		msg.add(""); //line 2
+		msg.add("§7"+Bukkit.getOnlinePlayers().size() + " / " + Bukkit.getMaxPlayers()); //Line 3
+		msg.add(""); //Line 4
 		commServer.SendMessage.sendToLobby(msg); //On envoie l'update au lobby
 	}
 	
