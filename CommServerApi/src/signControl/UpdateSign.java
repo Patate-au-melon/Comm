d93854225f70d4a964aa6b86ec9b5b9a736fd7e9
jeje2208 <baziinje@gmail.com>
2015-05-29 12:41:47 +0200

    Modification
    Modification du texte pour la forme generale sur la branche master

--- CommServerApi/src/signControl/UpdateSign.java
+++ CommServerApi/src/signControl/UpdateSign.java
@@ -18,11 +18,10 @@ public class UpdateSign {
 	
 	public static void updateSign(int nbPlayerOnServer){
 		ArrayList<String> msg = new ArrayList<>();
-		msg.add("signControl Update"); //On dit que l'on veut update un sign
-		msg.add("§1§lPvP - Box"); //Line 1
-		msg.add(""); //line 2
-		msg.add("§7"+ nbPlayerOnServer + " / " + Bukkit.getMaxPlayers()); //Line 3
-		msg.add(""); //Line 4
+		msg.add("test1"); //Line 1
+		msg.add("test2"); //line 2
+		msg.add("test3"); //Line 3
+		msg.add("test4"); //Line 4
 		commServer.SendMessage.sendToLobby(msg); //On envoie l'update au lobby
 	}
 	
