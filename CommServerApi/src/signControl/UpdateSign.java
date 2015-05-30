96560c18b3f2a5a8daa10dbde9894282a82d6282
jeje2208 <baziinje@gmail.com>
2015-05-30 15:11:56 +0200

    Debug
    Probleme : mauvais nombre de joueur lors de la deconnexion
    Ajout variable intermediaire

--- CommServerApi/src/signControl/UpdateSign.java
+++ CommServerApi/src/signControl/UpdateSign.java
@@ -9,19 +9,19 @@ import org.bukkit.event.player.PlayerQuitEvent;
 public class UpdateSign {
 	
 	public static void playerjoin(PlayerJoinEvent e){
-		updateSign();
+		updateSign(Bukkit.getOnlinePlayers().size());
 	}
 	
 	public static void playerQuit(PlayerQuitEvent e){
-		updateSign();
+		updateSign(Bukkit.getOnlinePlayers().size() -1);
 	}
 	
-	public static void updateSign(){
+	public static void updateSign(int nbPlayerOnServer){
 		ArrayList<String> msg = new ArrayList<>();
 		msg.add("signControl Update"); //On dit que l'on veut update un sign
 		msg.add("§1§lPvP - Box"); //Line 1
 		msg.add(""); //line 2
-		msg.add("§7"+Bukkit.getOnlinePlayers().size() + " / " + Bukkit.getMaxPlayers()); //Line 3
+		msg.add(""+ nbPlayerOnServer + " / " + Bukkit.getMaxPlayers()); //Line 3
 		msg.add(""); //Line 4
 		commServer.SendMessage.sendToLobby(msg); //On envoie l'update au lobby
 	}
